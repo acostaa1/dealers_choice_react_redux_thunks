6e38810406a989b59f4453bca5de0ff701ebde4c
Angel Acosta <angel.s.acosta1@gmail.com>
2022-03-14 01:59:19 -0400

    added ability to submit own movies

--- src/index.js
+++ src/index.js
@@ -9,21 +9,31 @@ class App extends React.Component {
     this.state = {
       movies: [],
       selectedMovie: [],
+      inputMovie: "",
     };
-    
+    this.titleChange = this.titleChange.bind(this);
   }
   //picks a movie from list of movies
   async getRandom() {
-      const movies = (await axios.get("/api/movies")).data;
-      const selectedMovie = movies[Math.floor(Math.random()*(movies.length))].title;
-      this.setState({selectedMovie})
+    const movies = (await axios.get("/api/movies")).data;
+    const selectedMovie =
+      movies[Math.floor(Math.random() * movies.length)].title;
+    this.setState({ selectedMovie });
   }
   async remove(movie) {
     await axios.delete(`/api/movies/${movie.id}`);
-    const movies = this.state.movies.filter((_movie) => _movie.id !== movie.id)
-    this.setState({movies})
+    const movies = this.state.movies.filter((_movie) => _movie.id !== movie.id);
+    this.setState({ movies });
   }
-  
+  async addMovie(title) {
+    const movie = (await axios.post(`/api/movies/${title}`)).data;
+    const movies = [...this.state.movies, movie];
+    this.setState({ movies });
+  }
+  titleChange(event) {
+    this.setState({ inputMovie: event.target.value });
+  }
+
   async componentDidMount() {
     // first compenent, axios call to get our movies from server <- from db
     try {
@@ -38,17 +48,34 @@ class App extends React.Component {
     return (
       <div>
         <div className="moviesList">
-          <h3>List of Movies Available</h3>
+          <h3>List of Movies Available Or Add Your Own</h3>
+          <form onSubmit={() => this.addMovie(this.state.inputMovie)}>
+            <input
+              placeholder="add a movie title here"
+              onChange={this.titleChange}
+            ></input>
+            <button type="submit" className="add">
+              Add Movie
+            </button>
+          </form>
           <ul>
             {movies.map((movie) => (
-              <li key={movie.id}>{movie.title}<button className="delete" onClick = {()=> this.remove(movie)}>X</button></li>
+              <li key={movie.id}>
+                {movie.title}
+                <button className="delete" onClick={() => this.remove(movie)}>
+                  X
+                </button>
+              </li>
             ))}
           </ul>
         </div>
 
         <div className="pickMovie">
-            <h3>Pick A Movie to Watch Tonight <button onClick = {()=> this.getRandom()}>Pick Movie</button></h3>
-            <ul>{this.state.selectedMovie}</ul>
+          <h3>
+            Can't Choose One?{" "}
+            <button onClick={() => this.getRandom()}>Decide for me</button>
+          </h3>
+          <ul>{this.state.selectedMovie}</ul>
         </div>
       </div>
     );
