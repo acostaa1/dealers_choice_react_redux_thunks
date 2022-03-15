e97188383ecacd45022edc13c46e00a2b517a52c
Angel Acosta <angel.s.acosta1@gmail.com>
2022-03-15 17:03:00 -0400

    combined reducers

--- src/store.js
+++ src/store.js
@@ -1,27 +1,66 @@
 
-import { createStore } from 'redux'; 
+import { createStore, combineReducers } from 'redux'; 
 
-const reducer = (state = {movies: [], selectedMovie: [], inputTitle: ''}, action) => {
+// const reducer = (state = {movies: [], selectedMovie: [], inputTitle: ''}, action) => {
+//     if(action.type === 'LOAD_MOVIES') {
+//          state = {...state, movies: action.movies}; 
+//         }
+//     if (action.type === "GET_RANDOM_MOVIE") {
+//          state = {...state, selectedMovie: action.selectedMovie}; }
+//     if (action.type === "REMOVE_MOVIE" ) {
+//         const movies = state.movies.filter(movie => movie.id !== action.movie.id);
+//         state = {...state, movies}
+//     }
+//     if (action.type === "ADD_MOVIE") {
+//         const movies = [...this.state.movies, action.movie]
+//         state = {...state, movies}
+//     }
+//     if (action.type === 'INPUT_TITLE') {
+//         state = {...state, inputTitle: action.inputTitle}
+//     }
+   
+//     console.log(action)
+//     return state;
+// }
+const moviesReducer = (state = [], action) => {
     if(action.type === 'LOAD_MOVIES') {
-         state = {...state, movies: action.movies}; 
-        }
-    if (action.type === "GET_RANDOM_MOVIE") {
-         state = {...state, selectedMovie: action.selectedMovie}; }
-    if (action.type === "REMOVE_MOVIE" ) {
-        const movies = state.movies.filter(movie => movie.id !== action.movie.id);
-        state = {...state, movies}
+       //console.log(action)
+        return action.movies
     }
     if (action.type === "ADD_MOVIE") {
-        const movies = [...this.state.movies, action.movie]
-        state = {...state, movies}
+        //console.log(action)
+        return action.movies
     }
-    if (action.type === 'INPUT_TITLE') {
-        state = {...state, inputTitle: action.inputTitle}
+    if (action.type === "REMOVE_MOVIE" ) {
+        const movies = state.filter(movie => movie.id !== action.movie.id);
+       // console.log(action)
+        return movies
     }
-   
-    console.log(action)
-    return state;
+    
+    return state 
 }
-const store = createStore(reducer);
 
+const selectedMovieReducer = (state= [], action) => {
+    if (action.type === "GET_RANDOM_MOVIE") {
+        //console.log(action)
+        return action.selectedMovie
+    }
+    return state
+}
+const inputTitleReducer = (state= '', action) => {
+    if(action.type === 'INPUT_TITLE') {
+        //console.log(action)
+        return action.inputTitle
+    }
+    
+    return state
+}
+const reducer = combineReducers({
+    movies: moviesReducer,
+    selectedMovie: selectedMovieReducer,
+    inputTitle: inputTitleReducer,
+    
+});
+const store = createStore(reducer);
+window.store = store
 export default store;
